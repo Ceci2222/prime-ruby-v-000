1937678b22bb830503b023d80bff79285a72e496
Marie Burns <mcburns2222@gmail.com>
2018-11-20 20:20:01 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(number)
     range = (2...number)
     range_array = range.to_a 
     range_array.select {|test_number| number % test_number}
-    range.array.size == 0
+    range_array.size == 0
   else
     false
   end
