c8dc188cf6c117294c47a1d01ca7cdb61473e13b
Marie Burns <mcburns2222@gmail.com>
2018-11-20 20:24:31 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,8 @@ def prime?(number)
   if number > 1
     range = (2...number)
     range_array = range.to_a 
-    divisible = range_array.select {|test_number| number % test_number}
-    divisible.size == 0
+    divisible_numbers = range_array.select {|test_number| number % test_number}
+    divisible_numbers.size == 0
   else
     false
   end
