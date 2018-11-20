410f36517101d5dd6c3d638f30555836d88666ec
Marie Burns <mcburns2222@gmail.com>
2018-11-20 20:16:42 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 def prime?(number)
   if number > 1
-  range = (2...number)
-  range_array = range.to_a 
-  range_array.select {|test_number| number % test_number}
+    range = (2...number)
+    range_array = range.to_a 
+    range_array.select {|test_number| number % test_number}
   if range_array.size == 0
     true 
   else
